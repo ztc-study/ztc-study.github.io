8301614b63882e500063cd74e1dc2d1684fb3ee5
ztc325818 <15708489575@163.com>
2022-08-31 22:47:13 +0800

    no message

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,29 @@
 module.exports = {
-  title: '前端学习记录',
-  description: '记录一些平时的学习记录',
+  title: "前端学习记录",
+  description: "记录一些平时的学习记录",
   themeConfig: {
     nav: [
-      { text: '首页', link: '/' },
-     
-    ]
-  }
-}
+      { text: "首页", link: "/" },
+      {
+        text: "vue相关",
+        link: "/vue/one",
+      },
+    ],
+    //侧边栏
+    sidebar: {
+      "/vue/": [
+        {
+          title: "vue初始化",
+          collapsable: false,
+          // children: ["", "/guide/getting-started", "/guide/markdown"],
+          path: "/vue/one",
+        },
+        {
+          title: "原理",
+          collapsable: false,
+          path: "/vue/two",
+        },
+      ],
+    },
+  },
+};
